c8bceb8deb98fa43b331ca50773c3a8871a7be35
Sergiy Shevchuk <sirjjjjjjj@gmail.com>
2022-01-24 20:31:32 +0200

    done the work

--- js/common.js
+++ js/common.js
@@ -5,11 +5,12 @@ document.addEventListener('DOMContentLoaded', function() {
 		slidesPerView: 3,
 		speed: 1800,
 		centeredSlides: true,
-})
+		navigation: {
+			nextEl: '.showcase-navigation__next',
+			prevEl: '.showcase-navigation__prev'
+		}
+	})
 
-
-
-
-document.querySelector('video').playbackRate = 4;
+	document.querySelector('video').playbackRate = 2
 
 })
